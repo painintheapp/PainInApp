d048a5a7d7a34791e5a2fd392cc5dd29707602c8
fahadhussain <fahad_hussain2@hotmail.com>
2020-09-08 06:55:44 +0500

    added other condition section

--- packages/app/src/containers/SearchConditions/selectedCondition.tsx
+++ packages/app/src/containers/SearchConditions/selectedCondition.tsx
@@ -3,11 +3,18 @@ import {
     Text,
     View,
     StyleSheet,
+    ScrollView,
+    TouchableOpacity
 } from 'react-native';
-import { Button, CheckBox, Icon } from "react-native-elements";
+import { Button, CheckBox, Icon, Input } from "react-native-elements";
 import { Header } from "../../components";
 interface SearchConditionsState {
     no: boolean;
+    addCondition: boolean;
+    searchcondition: string;
+    searchconditionlist: Array<any>;
+    searched: boolean;
+    selectedlist: Array<any>;
 }
 
 interface OwnProps {
@@ -17,21 +24,50 @@ interface OwnProps {
 
 type Props = OwnProps;
 
+var list: Array<any> = [
+    "fever",
+    "cough",
+    "sore throat",
+    "runny or stuffy nose",
+    "muscle or body aches",
+    "headaches",
+    "fatigue",
+    "runny or stuffy nose",
+    "muscle or body aches",
+    "headaches",
+    "fatigue",
+    "runny or stuffy nose",
+    "muscle or body aches",
+    "headaches",
+    "fatigue",
+]
+
 class SelectedConditionPage extends React.Component<Props, SearchConditionsState> {
     constructor(props: Props) {
         super(props);
 
         this.state = {
             no: true,
+            addCondition: false,
+            searchcondition: '',
+            searchconditionlist: [],
+            selectedlist: [],
+            searched: false
         };
     }
 
-
-    public componentDidMount() {
+    public componentDidMount = () => {
+        const Conditions = this.props.route.params.condition && this.props.route.params.condition[0];
+        this.setState(prevState => ({
+            ...prevState,
+            selectedlist: [...prevState.selectedlist, Conditions]
+        }));
     }
+
 
     public render() {
         const Conditions = this.props.route.params.condition && this.props.route.params.condition[0];
+        const { addCondition, searched, searchcondition } = this.state;
         return (
             <React.Fragment>
                 <Header
@@ -40,35 +76,80 @@ class SelectedConditionPage extends React.Component<Props, SearchConditionsState
                     rightIcon={true}
                     navigation={this.props.navigation} />
                 <View style={styles.container}>
-                    <View style={styles.topView}>
-                        <Text style={styles.heading}>{Conditions}</Text>
-                        <Text style={styles.subheading}>{"This condition has been added to your journal. Do you suffer from any other conditions?"}</Text>
-                        <CheckBox
-                            center
-                            title='Yes'
-                            checkedIcon='dot-circle-o'
-                            checkedColor={'#fff'}
-                            uncheckedColor={"#fff"}
-                            uncheckedIcon='circle-o'
-                            onPress={() => this.setState({ no: !this.state.no })}
-                            containerStyle={styles.toggleView}
-                            checked={!this.state.no}
-                        />
-                        <CheckBox
-                            center
-                            title='No'
-                            checkedIcon='dot-circle-o'
-                            checkedColor={'#fff'}
-                            uncheckedColor={"#fff"}
-                            containerStyle={styles.toggleView}
-                            onPress={() => this.setState({ no: !this.state.no })}
-                            uncheckedIcon='circle-o'
-                            checked={this.state.no}
-                        />
-                        <Button title="Accept" buttonStyle={styles.accept} containerStyle={styles.acceptButtonContainer} onPress={this.onAccept} />
-                    </View>
+                    {
+                        addCondition ?
+                            <React.Fragment>
+                                <Input
+                                    containerStyle={[styles.containerStyle, { width: '90%' }]}
+                                    inputStyle={[styles.inputStyle]}
+                                    inputContainerStyle={[styles.inputContainerStyle, { marginTop: -5 }]}
+                                    onChangeText={(value) => this.filtered('searchcondition', value)}
+                                    leftIcon={<Icon name="search" type="feather" color="#000" size={18} />}
+                                    value={searched ? "" : searchcondition}
+                                    placeholder={"Search"} />
+                                {
+                                    this.state.searchconditionlist && this.state.searchconditionlist.length ?
+                                        <ScrollView keyboardDismissMode='none' keyboardShouldPersistTaps="always" style={styles.searchView}>
+                                            {
+                                                this.state.searchconditionlist && this.state.searchconditionlist.map(item => {
+
+                                                    return (
+                                                        <TouchableOpacity onPress={() => {
+                                                            this.onChange('searchcondition', item)
+                                                            this.setState({ searchconditionlist: [], searched: true })
+                                                        }} style={styles.searchedItem}>
+                                                            <Text>{item ? item : 'No Product Found'}</Text>
+                                                        </TouchableOpacity>
+                                                    )
+                                                })
+                                            }
+                                            {
+                                                this.state.searchconditionlist == [] ? <Text>No product found</Text> : null
+                                            }
+                                        </ScrollView>
+                                        : null
+                                }
+                            </React.Fragment>
+                            :
+
+                            <View style={styles.topView}>
+                                <Text style={styles.heading}>{Conditions}</Text>
+                                <Text style={styles.subheading}>{"This condition has been added to your journal. Do you suffer from any other conditions?"}</Text>
+                                <CheckBox
+                                    center
+                                    title='Yes'
+                                    checkedIcon='dot-circle-o'
+                                    checkedColor={'#fff'}
+                                    uncheckedColor={"#fff"}
+                                    uncheckedIcon='circle-o'
+                                    onPress={() => this.setState({ no: !this.state.no })}
+                                    containerStyle={styles.toggleView}
+                                    checked={!this.state.no}
+                                />
+                                <CheckBox
+                                    center
+                                    title='No'
+                                    checkedIcon='dot-circle-o'
+                                    checkedColor={'#fff'}
+                                    uncheckedColor={"#fff"}
+                                    containerStyle={styles.toggleView}
+                                    onPress={() => this.setState({ no: !this.state.no })}
+                                    uncheckedIcon='circle-o'
+                                    checked={this.state.no}
+                                />
+                                <Button title="Accept" buttonStyle={styles.accept} containerStyle={styles.acceptButtonContainer} onPress={this.onAccept} />
+                            </View>
+                    }
                     <View style={styles.backView}>
-                        <Button title="" icon={<Icon name="caretleft" type="antdesign" />} buttonStyle={styles.backCaret} containerStyle={styles.buttonContainer} onPress={this.goBack} />
+                        <Button title="" icon={<Icon name="caretleft" type="antdesign" />} buttonStyle={styles.backCaret} onPress={this.goBack} />
+                        {
+                            (addCondition && searched) &&
+                            <Text style={styles.selectedCondition}>{searchcondition}</Text>
+                        }
+                        {
+                            (addCondition && searched) &&
+                            <Button title="" icon={<Icon name="caretright" type="antdesign" />} buttonStyle={styles.nextCaret} />
+                        }
                     </View>
                 </View>
             </React.Fragment>
@@ -81,30 +162,71 @@ class SelectedConditionPage extends React.Component<Props, SearchConditionsState
 
     private onAccept = () => {
         const { no } = this.state;
-        const Conditions = this.props.route.params.condition;
         if (!no) {
-            this.props.navigation.navigate('SearchConditions', { Conditions: Conditions });
+            this.setState({ addCondition: true })
         } else {
         }
+    }
+
+    private onChange = (name: string, value: any) => {
+        this.setState(prevState => ({
+            ...prevState,
+            [name]: value,
+            selectedlist: [...prevState.selectedlist, value]
+        }));
+    }
+
+    private filtered = (name: string, value: string) => {
+        this.setState(prevState => ({
+            ...prevState,
+            [name]: value,
+            searched: false
+        }), () => {
+            let state = this.state[name];
+            if (state) {
+                let text = state.toLowerCase()
+                let filteredName = list.filter((item) => {
+                    return item.toLowerCase().match(text)
+                })
+                if (!text || text === '') {
+                    this.setState(prevState => ({
+                        ...prevState,
+                        [name + 'list']: list,
+                    }));
+                } else if (Array.isArray(filteredName)) {
+                    this.setState(prevState => ({
+                        ...prevState,
+                        [name + 'list']: filteredName,
+                    }));
+                }
+            } else {
+                this.setState(prevState => ({
+                    ...prevState,
+                    [name + 'list']: list,
+                }));
+            }
+        });
     }
 }
 
 
 const styles = StyleSheet.create({
     container: { backgroundColor: '#93BEF2', flex: 1, alignItems: 'center', justifyContent: 'space-between', paddingVertical: 20 },
-    inputView: { width: '90%', justifyContent: 'space-between', alignItems: 'center', flexDirection: 'row', marginBottom: 20 },
     heading: { color: '#000', fontSize: 16, fontWeight: 'bold' },
     subheading: { color: '#000', fontSize: 14, marginBottom: 30, marginTop: 10, textAlign: 'center' },
-    doyouknow: { color: '#fff', fontSize: 14, marginBottom: 30, marginTop: 10 },
     topView: { width: '90%', alignItems: 'center' },
     acceptButtonContainer: { width: '75%', marginTop: 20 },
     accept: { backgroundColor: '#035EC7', borderRadius: 5 },
-    buttonContainer: { marginTop: 20 },
-    backCaret: { backgroundColor: 'transparent', borderRadius: 5 },
-    toggleView: { backgroundColor: 'transparent', borderWidth: 0 },
-    backView: { width: '90%', alignItems: 'flex-start' },
+    containerStyle: { backgroundColor: '#fff', height: 40, alignItems: 'center' },
+    inputContainerStyle: { borderBottomWidth: 0 },
+    inputStyle: { fontSize: 14, width: '100%', height: 40, color: '#000080', },
     searchView: { zIndex: 20, maxHeight: '70%', elevation: 3, borderWidth: 0, width: '90%', borderRadius: 0, marginBottom: 20, backgroundColor: '#fff' },
-    searchedItem: { width: '100%', alignItems: 'flex-start', height: 35, justifyContent: 'center', paddingLeft: 10, borderBottomWidth: 1, borderBottomColor: '#d7d7d7' }
+    searchedItem: { width: '100%', alignItems: 'flex-start', height: 35, justifyContent: 'center', paddingLeft: 10, borderBottomWidth: 1, borderBottomColor: '#d7d7d7' },
+    toggleView: { backgroundColor: 'transparent', borderWidth: 0 },
+    backCaret: { backgroundColor: 'transparent', borderRadius: 5 },
+    nextCaret: { backgroundColor: '#228B22', borderRadius: 20 },
+    selectedCondition: { color: '#228B22' },
+    backView: { width: '90%', alignItems: 'center', flexDirection: 'row', justifyContent: 'space-between' },
 })
 
 export const SelectedCondition = SelectedConditionPage;
